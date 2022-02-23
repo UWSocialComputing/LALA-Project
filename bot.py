678b89a1b917006a6891f1fe1d03084f89d80d4f
Lynda <lpham12@cs.washington.edu>
2022-02-23 14:07:27 -0800

    Create embed bot response and validate emoji reacts to embeds

--- bot.py
+++ bot.py
@@ -13,4 +13,30 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!embed'):
+        embed=discord.Embed(title="Sample Embed", url="https://realdrewdata.medium.com/", description="This is an embed that will show how to build an embed and the different components", color=0xFF5733)
+        embed.set_footer(text="poll")
+        await message.channel.send(embed=embed)
+
+allowed_reacts = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
+@client.event
+async def on_reaction_add(reaction, user):
+    msg = reaction.message
+    if len(msg.embeds)>0:
+        emb = msg.embeds[0]
+        ft = emb.footer
+        if ft.text.startswith('poll'):
+            print('This is a poll')
+            await msg.channel.send("got a reaction")
+            if reaction.emoji not in allowed_reacts:
+                print('invalid')
+                await reaction.clear()
+            else:
+                print('valid emoji')
+
 client.run(TOKEN)
