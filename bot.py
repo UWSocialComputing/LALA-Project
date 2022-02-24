b59795954e43b06db9b738b3a65067c779aec959
audreyma <ama50@cs.washington.edu>
2022-02-24 00:16:26 -0800

    Finish study session scheduling

--- bot.py
+++ bot.py
@@ -1,55 +1,79 @@
 # bot.py
 import os
-
 import discord
 from dotenv import load_dotenv
-
+from discord.ext import commands
 from StudySession import StudySession
 import datetime
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+client = commands.Bot(command_prefix='/')
 
+study_session_users = []
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
     print(discord.__version__)
+@client.command(name='schedule')
+async def schedule_session(ctx, *args):
+    study_session = parse_study_session_request(*args)
+    await print_study_session_request_response(ctx, study_session)
 
-# Triggers every time a message is received
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('/schedule'):
-        study_session = parse_study_session_request(message)
-        await print_study_session_request_response(message, study_session)
+@client.command(name='startsession')
+async def start_session(ctx, arg):
+    channel = await ctx.guild.create_text_channel(arg)
+    user_string = ''
+    for user in study_session_users:
+        user_string += f'<@{user.id}> '
+    await channel.send(user_string + 'your study session is starting now!')
+  
 
-def parse_study_session_request(message):
-    study_session_parse_string = message.content.split(' ')
+def parse_study_session_request(*message):
+    print(*message)
 
     # Assumes date string is given in the format of YYYY-MM-DD
-    study_date = study_session_parse_string[1]
+    study_date = message[0]
     study_date = datetime.date.fromisoformat(study_date)
 
     # TODO: parse time correctly
-    study_time = study_session_parse_string[2]
+    study_time = message[1]
 
     # parse duration: either 1, 2, or 3 hours
-    duration = study_session_parse_string[3]
+    duration = message[2]
 
     study_session = StudySession(study_date, study_time, duration)
     return study_session
 
+
+# TODO: Method to start the study session
+def start_study_session():
+    return
+
 async def print_study_session_request_response(message, study_session):
-    await message.channel.send(f'{message.author} has requested a study session!')
-    await message.channel.send(f'on {study_session.date} at {study_session.time} for {study_session.duration} hour')
+    # TODO: Find a way to format the message so it can @channel
+    embed=discord.Embed(title=f'{message.author} has requested a study session on {study_session.date} at {study_session.time} for {study_session.duration} hour',
+                         description='Please use the emojis to accept or reject this study session!',
+                        color=0xFF5733)
+    embed.set_footer(text="schedule poll")
+    await message.channel.send(embed=embed)
+   
 
 @client.event
 async def on_member_join(member):
     await member.send("Welcome! TEST")
+
+@client.event
+async def on_reaction_add(reaction, user):
+    msg = reaction.message
+    if len(msg.embeds)>0:
+        emb = msg.embeds[0]
+        ft = emb.footer
+        if ft.text.startswith('schedule poll'):
+            if reaction.emoji == '✅':
+                study_session_users.append(user)
+                print(user)
     
 client.run(TOKEN)
